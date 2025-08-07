3887fb07a329aaca40623f814847bb774ad6f923
Файни <grishalejter271@gmail.com>
2025-08-08 02:06:04 +0300

    Changes to be committed: new file:   handlers/start.py
    - Написал хэндлер команды старт
    
    - добавил callback кнопку

---
+++ handlers/start.py
@@ -0,0 +1,20 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+start_cmd = Router()
+
+@start_cmd.message(CommandStart(deep_link=True))
+async def start_cmd(message: Message, command: CommandStart):
+    args = command.args
+    if args == "reg":
+        reg_butt = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton(text="Записатся", callback_data="register")]
+            ])
+        await message.answer(
+            "Привет\n\n"
+            "Нажмите кнопку ниже, чтобы начать.",
+            reply_markup=reg_butt)
+    else:
+        await message.answer("Перейдите по кнопке из телеграм канала")
