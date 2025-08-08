915237fa723d2e55f3ec8e6595812c3ae7d2b945
Файни <grishalejter271@gmail.com>
2025-08-09 00:59:01 +0300

    работает таблица

--- sheets/sync.py
+++ sheets/sync.py
@@ -4,32 +4,28 @@ from pathlib import Path
 import gspread
 from google.oauth2.service_account import Credentials
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-DB = BASE_DIR / "database" / "registrs.db"
 TAB_NAME = "registrations"
+BASE_DIR = Path(__file__).resolve().parent.parent
+DB_PATH = BASE_DIR / "database" / "registrs.db"
 
 SHEET_ID = '1Qpm2cakUmtEqe4g7AI6U4S1tb2AyLCkRCBZ3cdYPgXc'
-SHEET_NAME = '1'
+SHEET_NAME = 'Sheet1'
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-CREDS_FILE = 'cred.json'
+CREDS_FILE = Path(__file__).resolve().parent / 'cred.json'
 
 def get_data_from_db():
-    conn = sqlite3.connect('../database/registrs.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT name, phone, usrname FROM registrations")
-    data = cursor.fetchall()
-    conn.close()
-    return data
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT name, phone, usrname FROM registrations")
+        return cursor.fetchall()
 
 def update_google_sheet(data):
-    creds = Credentials.from_service_account_file(CREDS_FILE, scopes=SCOPES)
+    creds = Credentials.from_service_account_file(str(CREDS_FILE), scopes=SCOPES)
     client = gspread.authorize(creds)
     sheet = client.open_by_key(SHEET_ID).worksheet(SHEET_NAME)
-
     sheet.clear()
     sheet.append_row(['name', 'phone', 'usrname'])
-
     if data:
         sheet.append_rows(data)
 
@@ -37,7 +33,7 @@ def main():
     while True:
         data = get_data_from_db()
         update_google_sheet(data)
-        time.sleep(600)  #10 минуток
+        time.sleep(600)
 
 if __name__ == "__main__":
     main()
