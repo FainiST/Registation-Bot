ea7b9cdb8eb33c78dfb2673b4ae9f6b09757b75a
Файни <grishalejter271@gmail.com>
2025-08-08 20:15:57 +0300

    база данных только databse.db без меин

--- database/db.py
+++ database/db.py
@@ -2,3 +2,27 @@ import sqlite3
 from pathlib import Path
 
 DB = Path(__file__).parent / "registrs.db"
+
+def init_db():
+    with sqlite3.connect(DB) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS registrations (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            phone TEXT NOT NULL,
+            usrname TEXT,
+            chat_id INTEGER NOT NULL
+        )
+        """)
+
+        conn.commit()
+
+def add_registration(name: str, phone: str, usrname: str, chat_id: int):
+    with sqlite3.connect(DB) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+        INSERT INTO registrations (name, phone, usrname, chat_id)
+        VALUES (?, ?, ?, ?)
+        """, (name, phone, usrname, chat_id))
+        conn.commit()
